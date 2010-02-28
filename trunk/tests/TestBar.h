72f75505863da26f25b8636ca294b7e56f8eb719
arthur.ii.yuan <arthur.ii.yuan@09cf5d66-22f6-11df-bff0-d338b04bc40e>
2010-02-28 09:26:04 +0000

    added mock test

--- trunk/tests/TestBar.h
+++ trunk/tests/TestBar.h
@@ -1,7 +1,10 @@
 
 #include <testngpp/testngpp.hpp>
+#include <mockcpp/mokc.h>
 
 #include <CBar.h>
+
+USING_MOCKCPP_NS
 
 FIXTURE(CBar)
 {
@@ -19,5 +22,21 @@ FIXTURE(CBar)
 	TEST(if devident is 0 => throw exception)
 	{
 		WARN("not implemented yet");
+	}
+
+	TEST(test devide by 0 with mock)
+	{
+		Except ex;
+		MOCKER(CBar::divide).stubs().with(any(), eq(0)).will(throws(ex));
+		
+		__DO__
+
+		ASSERT_THROWS(CBar::divide(1,0), Except);
+
+		__CLEANUP__
+
+		GlobalMockObject::reset();
+
+		__DONE__
 	}
 };
